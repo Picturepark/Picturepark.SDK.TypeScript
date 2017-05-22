e46a09ed787950c965e83c029d327ba2d9930e28
Rico Suter <mail@rsuter.com>
2017-05-22 16:18:04 +0200

    Fixed build script

--- build/06_BuildProduction.bat
+++ build/06_BuildProduction.bat
@@ -1,14 +1,14 @@
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2"
 cmd /c npm install --loglevel=error
-cmd /c "node_modules/.bin/ng" build --prod
+cmd /c call "node_modules/.bin/ng" build --prod
 popd
 
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2-ui"
 cmd /c npm install --loglevel=error
-cmd /c "node_modules/.bin/ng" build --prod
+cmd /c call "node_modules/.bin/ng" build --prod
 popd
 
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2-app"
 cmd /c npm install --loglevel=error
-cmd /c "node_modules/.bin/ng" build --prod
+cmd /c call "node_modules/.bin/ng" build --prod
 popd
