1cc587e4e24d4de9cea0648839768742413849ee
Rico Suter <mail@rsuter.com>
2017-05-22 15:49:04 +0200

    Updated build script

--- build/06_BuildProduction.bat
+++ build/06_BuildProduction.bat
@@ -1,14 +1,14 @@
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2"
 cmd /c npm install
-cmd /c ng build --prod
+cmd /c "node_modules/.bin/ng" build --prod
 popd
 
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2-ui"
 cmd /c npm install
-cmd /c ng build --prod
+cmd /c "node_modules/.bin/ng" build --prod
 popd
 
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2-app"
 cmd /c npm install
-cmd /c ng build --prod
+cmd /c "node_modules/.bin/ng" build --prod
 popd
