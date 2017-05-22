80097b12e067735d45f743abc8d19508d19ac825
Rico Suter <mail@rsuter.com>
2017-05-22 15:08:27 +0200

    Updated test script

--- build/02_RunTests.bat
+++ build/02_RunTests.bat
@@ -1,14 +1,14 @@
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2"
 cmd /c npm install
-cmd /c npm run ng -- test -w false
+cmd /c call "node_modules/.bin/ng" test -w false
 popd
 
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2-ui"
 cmd /c npm install
-cmd /c npm run ng -- test -w false
+cmd /c call "node_modules/.bin/ng" test -w false
 popd
 
 pushd "%~dp0/../src/picturepark-sdk-v1-ng2-app"
 cmd /c npm install
-cmd /c npm run ng -- test -w false
+cmd /c call "node_modules/.bin/ng" test -w false
 popd
