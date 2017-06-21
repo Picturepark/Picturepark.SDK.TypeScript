59dbeec35e785a75d55eff82f90f461f622c0ad7
Rico Suter <mail@rsuter.com>
2017-06-21 15:29:05 +0200

    Added sample angular project

---
+++ samples/picturepark-sdk-v1-sample-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { PICTUREPARK_URL } from '@picturepark/sdk-v1-angular';
+import { PictureparkUiModule } from '@picturepark/sdk-v1-angular-ui';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    PictureparkUiModule
+  ],
+  providers: [
+    { provide: PICTUREPARK_URL, useValue: "https://devnext.preview-picturepark.com" }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
