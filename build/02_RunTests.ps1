94cc56bdf636ce6e9ea93edf9091068de490148b
Rico Suter <mail@rsuter.com>
2017-09-21 13:34:58 +0200

    Added test fail detection

--- build/02_RunTests.ps1
+++ build/02_RunTests.ps1
@@ -1,9 +1,12 @@
 if (!(Test-Path "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.ts")) { 
-	(Get-Content "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.template.ts") | 
-	ForEach-Object { $_ -replace "{Server}", "$env:TestServer" } | 
-	ForEach-Object { $_ -replace "{Username}", "$env:TestUsername" } | 
-	ForEach-Object { $_ -replace "{Password}", "$env:TestPassword" } | 
-	Set-Content "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.ts"
+    (Get-Content "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.template.ts") | 
+        ForEach-Object { $_ -replace "{Server}", "$env:TestServer" } | 
+        ForEach-Object { $_ -replace "{Username}", "$env:TestUsername" } | 
+        ForEach-Object { $_ -replace "{Password}", "$env:TestPassword" } | 
+        Set-Content "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.ts"
 }
 
 cmd /c "$PSScriptRoot/02_RunTests.bat"
+if ($lastexitcode -ne 0) {
+    throw "Failed to run unit tests"
+}
