710d8b3bf85b82380ecf0ad3447ca4dfbd47715d
Rico Suter <mail@rsuter.com>
2017-09-21 18:36:21 +0200

    Do not run packages without tests

--- build/02_RunTests.bat
+++ build/02_RunTests.bat
@@ -2,13 +2,13 @@ pushd "%~dp0/../src/picturepark-sdk-v1-angular"
 cmd /c call "node_modules/.bin/ng" test --watch=false || goto :error
 popd
 
-pushd "%~dp0/../src/picturepark-sdk-v1-angular-ui"
-cmd /c call "node_modules/.bin/ng" test --watch=false || goto :error
-popd
+REM pushd "%~dp0/../src/picturepark-sdk-v1-angular-ui"
+REM cmd /c call "node_modules/.bin/ng" test --watch=false || goto :error
+REM popd
 
-pushd "%~dp0/../src/picturepark-sdk-v1-angular-app"
-cmd /c call "node_modules/.bin/ng" test --watch=false || goto :error
-popd
+REM pushd "%~dp0/../src/picturepark-sdk-v1-angular-app"
+REM cmd /c call "node_modules/.bin/ng" test --watch=false || goto :error
+REM popd
 
 goto :EOF
 :error
