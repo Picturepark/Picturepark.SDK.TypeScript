56366fa63f409091be6e491718e2a5bfd0c4dd04
Urs Brogle <ub@picturepark.com>
2017-12-22 14:18:17 +0100

    Align test configuration with .net sdk.

--- build/02_RunTests.ps1
+++ build/02_RunTests.ps1
@@ -26,7 +26,7 @@ try {
 	
     if (!(Test-Path "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.ts")) { 
         (Get-Content "$PSScriptRoot/../src/picturepark-sdk-v1-angular/src/tests/config.template.ts") | 
-            ForEach-Object { $_ -replace "{Server}", "$env:TestServer" } | 
+            ForEach-Object { $_ -replace "{Server}", "$env:TestApiServer" } | 
             ForEach-Object { $_ -replace "{Username}", "$env:TestUsername" } | 
             ForEach-Object { $_ -replace "{Password}", "$env:TestPassword" } | 
             ForEach-Object { $_ -replace "{AccessToken}", "$env:TestAccessToken" } | 
