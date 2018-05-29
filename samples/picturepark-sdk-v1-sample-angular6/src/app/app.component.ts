613073e7c344d49f234b79cdf9f4ac7eaa78bd09
Urs Brogle <ub@picturepark.com>
2018-05-29 08:41:08 +0200

    Added angular 6 example. Removed angular 4 + 5 examples. Updated logo.

---
+++ samples/picturepark-sdk-v1-sample-angular6/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { PublicAccessService } from '@picturepark/sdk-v1-angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  output: string;
+
+  token = '';
+  currentToken = '';
+
+  constructor(public publicAccessService: PublicAccessService) {
+  }
+
+  async load() {
+    this.output = 'Loading...';
+    this.currentToken = this.token;
+    if (this.currentToken) {
+      this.publicAccessService.getShare(this.currentToken).subscribe(response => {
+        this.output = JSON.stringify(response, null, 4);
+      }, error => {
+        this.output = error.toString();
+      });
+    } else {
+      this.output = '';
+    }
+  }
+}
