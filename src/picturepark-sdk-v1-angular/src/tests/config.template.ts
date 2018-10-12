c081a5dec65ba334ff1cc691089c08d8e55188cf
Urs Brogle <ub@picturepark.com>
2018-10-12 20:28:55 +0200

    PP9-6627: Fix test template.

--- src/picturepark-sdk-v1-angular/src/tests/config.template.ts
+++ src/picturepark-sdk-v1-angular/src/tests/config.template.ts
@@ -4,10 +4,10 @@ import {
   PICTUREPARK_API_URL,
   PICTUREPARK_CONFIGURATION,
   PictureparkConfiguration,
+  AuthService,
   AccessTokenAuthService
 } from '@picturepark/sdk-v1-angular';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from 'projects/picturepark-sdk-v1-angular/src/public_api';
 
 export const testUrl = '{Server}';
 export const testAccessToken = '{AccessToken}';
