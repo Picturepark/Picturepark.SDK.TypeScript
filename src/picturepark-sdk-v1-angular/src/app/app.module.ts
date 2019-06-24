3fdd1a13a2037246084d70884b6e8674da3f8e60
Francisco Roca <f.roca@ad.vit.ch>
2019-06-24 11:51:00 +0200

    PP9-8518 modularization

--- src/picturepark-sdk-v1-angular/src/app/app.module.ts
+++ src/picturepark-sdk-v1-angular/src/app/app.module.ts
@@ -1,36 +1,28 @@
-import { DetailsDialogComponent } from './details-dialog/details-dialog.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContentPickerComponent } from './content-picker/content-picker.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
-import {
-  PictureparkOidcAuthConfiguration,
-  PictureparkOidcModule
-} from '@picturepark/sdk-v1-angular-oidc';
+// LIBRARIES
+import { PictureparkOidcAuthConfiguration, PictureparkOidcModule } from '@picturepark/sdk-v1-angular-oidc';
 import { PictureparkUiModule } from '@picturepark/sdk-v1-angular-ui';
 import { PICTUREPARK_CONFIGURATION } from '@picturepark/sdk-v1-angular';
+
+// MODULES
+import { AppRoutingModule } from './app-routing.module';
+
+
+// COMPONENTS
+import { AppComponent } from './app.component';
+import { ContentPickerComponent } from './content-picker/content-picker.component';
+import { DetailsDialogComponent } from './details-dialog/details-dialog.component';
+import { HomeComponent } from './home/home.component';
 
 export function LocaleIdFactory() {
   return (<any>navigator).languages ? (<any>navigator).languages[0] : navigator.language;
 }
 
+// CLIENT CONFIG
 export function PictureparkConfigurationFactory() {
   const appRootTag = document.getElementsByTagName('app-root')[0];
   return <PictureparkOidcAuthConfiguration>{
@@ -45,7 +37,6 @@ export function PictureparkConfigurationFactory() {
 }
 
 @NgModule({
-  entryComponents: [DetailsDialogComponent],
   declarations: [
     AppComponent,
     HomeComponent,
@@ -54,33 +45,24 @@ export function PictureparkConfigurationFactory() {
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
 
-    MatToolbarModule,
-    MatTabsModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatBadgeModule,
-    MatButtonModule,
-    MatCardModule,
-    LayoutModule,
-
     PictureparkUiModule,
-    PictureparkOidcModule,
-    RouterModule.forRoot([
+    PictureparkOidcModule
+    /*RouterModule.forRoot([
       { path: '', component: AppComponent },
       { path: 'details', component: AppComponent },
       { path: 'pcpToken/:type?postUrl=:postUrl', redirectTo: '/content-picker?postUrl=:postUrl' }
-    ])
+    ]),*/
   ],
   providers: [
     { provide: LOCALE_ID, useFactory: LocaleIdFactory },
     { provide: PICTUREPARK_CONFIGURATION, useFactory: PictureparkConfigurationFactory }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DetailsDialogComponent]
 })
 export class AppModule {
 }
