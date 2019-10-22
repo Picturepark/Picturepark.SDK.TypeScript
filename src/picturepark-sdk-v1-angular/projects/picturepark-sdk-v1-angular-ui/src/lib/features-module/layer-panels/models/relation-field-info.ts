2c8fed0fa73bfc2c9659c82a2f9b8020b7917d95
Urs Brogle <ub@picturepark.com>
2019-10-22 18:18:56 +0200

    PP9-9459: Show relation content title / description instead of filename, fix layout.

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/layer-panels/models/relation-field-info.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/layer-panels/models/relation-field-info.ts
@@ -2,12 +2,14 @@ import { SafeUrl } from '@angular/platform-browser';
 
 export class RelationFieldInfo {
   public contentId: string;
-  public fileName?: string;
+  public name?: string;
+  public list?: string;
   public fileUrl?: SafeUrl;
 
-  constructor(contentId: string, fileName?: string, fileUrl?: SafeUrl) {
+  constructor(contentId: string, name?: string, list?: string, fileUrl?: SafeUrl) {
     this.contentId = contentId;
-    this.fileName = fileName;
+    this.name = name;
+    this.list = list;
     this.fileUrl = fileUrl;
   }
 }
