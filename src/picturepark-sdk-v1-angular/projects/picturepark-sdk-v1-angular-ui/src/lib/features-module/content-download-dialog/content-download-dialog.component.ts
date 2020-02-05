f469c7db8f48ea1110c6e8336607b3405d75cbce
Urs Brogle <ub@picturepark.com>
2020-02-05 14:14:22 +0100

    Code cleanup.

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/content-download-dialog/content-download-dialog.component.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/content-download-dialog/content-download-dialog.component.ts
@@ -100,7 +100,7 @@ export class ContentDownloadDialogComponent extends DialogBaseComponent implemen
     });
 
     this.selection = selection;
-    this.noOutputs = !selection.getFileFormats().some(i => selection.getOutputs(i).length > 0);
+    this.noOutputs = !fileFormats.some(i => selection.getOutputs(i).length > 0);
     this.singleItem = fileFormats.length === 1 && fileFormats[0].contents.length === 1;
   }
 
@@ -185,7 +185,7 @@ export class ContentDownloadDialogComponent extends DialogBaseComponent implemen
     if (this.data.contents.length === 1) {
       const detail = (this.data.contents[0] as ContentDetail);
       if (detail.outputs) {
-        await this.setSelection(detail.outputs!);
+        await this.setSelection(detail.outputs);
         return;
       }
 
