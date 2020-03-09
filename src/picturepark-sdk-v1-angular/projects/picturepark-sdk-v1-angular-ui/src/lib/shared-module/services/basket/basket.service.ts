038d4655e0d5b24e994a0760717f9c47426acb62
Ricardo Santos <ricardojoaosantos0@gmail.com>
2020-03-09 16:09:09 +0000

    Fix backwards compatibility

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/shared-module/services/basket/basket.service.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/shared-module/services/basket/basket.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Content } from '@picturepark/sdk-v1-angular';
+import { Content,
+  ContentService,
+  fetchAll,
+  ContentSearchRequest,
+  LifeCycleFilter,
+  BrokenDependenciesFilter,
+  ContentSearchType,
+  TermsFilter } from '@picturepark/sdk-v1-angular';
+import { ContentResolveBehavior } from '@picturepark/sdk-v1-angular';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +20,7 @@ export class BasketService {
   private localStorageKey = 'basketItems';
   public basketItems: Content[];
 
-  constructor() {
+  constructor(private contentService: ContentService) {
     const storedItem = localStorage.getItem(this.localStorageKey);
     this.basketItems = storedItem ? JSON.parse(storedItem) as Content[] : [];
     this.basketSubject = new BehaviorSubject(this.basketItems);
@@ -23,15 +31,55 @@ export class BasketService {
     return this.basketSubject.asObservable();
   }
 
-  public addItem(item: Content) {
-    if (this.basketItems.findIndex( q => q.id === item.id ) === -1) {
-      this.basketItems.push(item);
+  /**
+   * @param item The item to be stored in the basket, it can be either the item in itself or
+   * just the item id (utilizing the item id will create the need for aditional api requests)
+   */
+  public addItem(item: Content|string) {
+    if (typeof(item) === 'string') {
+      this.addItemById(item);
+    } else {
+      if (this.basketItems.findIndex( q => q.id === item.id ) === -1) {
+        this.basketItems.push(item);
+        this.updateStorage();
+      }
+    }
+  }
+
+  public addItemById(item: string) {
+    fetchAll(req => this.contentService.search(req), new ContentSearchRequest({
+      limit: 1000,
+      lifeCycleFilter: LifeCycleFilter.ActiveOnly,
+      brokenDependenciesFilter: BrokenDependenciesFilter.All,
+      searchType: ContentSearchType.MetadataAndFullText,
+      debugMode: false,
+      filter: new TermsFilter({
+        field: 'id',
+        terms: [item]
+      })
+    })).subscribe( result => {
+      if (result[0]) {
+        this.basketItems.push(result[0]);
+        this.updateStorage();
+      }
+    });
+  }
+
+  /**
+   * @param item The item to be removed from the basket, it can be either the item in itself or
+   * just the item id
+   */
+  public removeItem(item: Content|string) {
+    if (typeof(item) === 'string') {
+      this.removeItemById(item);
+    } else {
+      this.basketItems = this.basketItems.filter( q => q.id !== item.id);
       this.updateStorage();
     }
   }
 
-  public removeItem(item: Content) {
-    this.basketItems = this.basketItems.filter( q => q.id !== item.id);
+  public removeItemById(item: Content|string) {
+    this.basketItems = this.basketItems.filter( q => q.id !== item);
     this.updateStorage();
   }
 
