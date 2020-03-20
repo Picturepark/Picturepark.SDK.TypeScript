e39b422cc043e82a09789c588b8d75406839885b
Urs Brogle <ub@picturepark.com>
2020-03-20 15:26:03 +0100

    PP9-9089: Use localStorage, fix merge.

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-oidc/src/lib/picturepark-oidc.module.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-oidc/src/lib/picturepark-oidc.module.ts
@@ -1,11 +1,15 @@
-import { NgModule, Inject } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AuthService } from '@picturepark/sdk-v1-angular';
 import { OidcAuthService } from './oidc-auth.service';
-import { OAuthModule } from 'angular-oauth2-oidc';
+import { OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
 
 // IMPORTANT: Update docs/picturepark-sdk-v1-angular/modules.md when changing modules
+
+export function storageFactory(): OAuthStorage {
+  return localStorage;
+}
 
 @NgModule({
   imports: [
@@ -13,6 +17,7 @@ import { OAuthModule } from 'angular-oauth2-oidc';
     OAuthModule.forRoot(),
   ],
   providers: [
+    { provide: OAuthStorage, useFactory: storageFactory },
     { provide: AuthService, useClass: OidcAuthService }
   ]
 })
