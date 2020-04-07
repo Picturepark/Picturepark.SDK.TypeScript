3281a37c20ae8e0420a18d100cab8c1e3933c3f1
Urs Brogle <ub@picturepark.com>
2020-04-07 11:24:44 +0200

    PP9-10588: Try to fix startup.

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular/src/lib/services/customer-info.service.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular/src/lib/services/customer-info.service.ts
@@ -2,30 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable, Optional } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
-import { CustomerInfo, PICTUREPARK_API_URL } from './api-services';
+import { CustomerInfo } from './api-services';
 import { PICTUREPARK_CONFIGURATION } from './base.service';
-import { PictureparkOidcAuthConfiguration } from '@picturepark/sdk-v1-angular-oidc';
+import { PictureparkConfiguration } from '../models/configuration';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerInfoService {
-  protected jsonParseReviver: ((key: string, value: any) => any) | undefined = undefined;
-
   constructor(
     @Inject(HttpClient) private http: HttpClient,
-    @Optional() @Inject(PICTUREPARK_API_URL) private baseUrl: string,
-    @Inject(PICTUREPARK_CONFIGURATION) private pictureparkConfiguration?: PictureparkOidcAuthConfiguration
-    ) {}
+    @Optional() @Inject(PICTUREPARK_CONFIGURATION) private config: PictureparkConfiguration
+  ) {}
 
   public getInfo(): Observable<CustomerInfo> {
-    let url_ = this.pictureparkConfiguration?.apiServer + '/v1/Info/customer';
+    let url_ = this.config.apiServer + '/v1/Info/customer';
     url_ = url_.replace(/[?&]$/, '');
 
-    return this.http.get<CustomerInfo>(url_).pipe(
-      mergeMap((response_: any) => {
-        return of(CustomerInfo.fromJS(response_));
-      })
-    );
+    return this.http
+      .get<CustomerInfo>(url_, { headers: { 'Picturepark-CustomerAlias': this.config.customerAlias! } })
+      .pipe(
+        mergeMap((response_: any) => {
+          return of(CustomerInfo.fromJS(response_));
+        })
+      );
   }
 }
