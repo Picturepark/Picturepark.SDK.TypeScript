0f8f43da510a41a19c9df83abf5355f42854fd70
Urs Brogle <ub@picturepark.com>
2020-04-16 17:14:20 +0200

    PP9-10693: Regenerate and add missing VectorMetadata handling.

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/utilities/constants.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/utilities/constants.ts
@@ -4,6 +4,7 @@ export const NON_VIRTUAL_CONTENT_SCHEMAS_IDS = [
   'FileMetadata',
   'ImageMetadata',
   'VideoMetadata',
+  'VectorMetadata',
 ];
 
 export const BROKEN_IMAGE_URL = 'https://icons-for-free.com/download-icon-broken+image+48px-131985226047038454_512.png';
