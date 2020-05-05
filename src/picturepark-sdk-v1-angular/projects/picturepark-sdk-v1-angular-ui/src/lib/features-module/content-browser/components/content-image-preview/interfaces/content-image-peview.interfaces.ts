bef3a3f954b3920c73b17b21b314b6d97ce49245
Ricardo Santos <ricardojoaosantos0@gmail.com>
2020-05-05 15:41:47 +0100

    Fix debouncing of full screen and image display

---
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/content-browser/components/content-image-preview/interfaces/content-image-peview.interfaces.ts
@@ -0,0 +1,6 @@
+import { IShareItem } from '../../../../content-details-dialog/fullscreen.service';
+
+export interface FullScreenDisplayItems {
+  selectedItem: IShareItem;
+  items: IShareItem[];
+}
