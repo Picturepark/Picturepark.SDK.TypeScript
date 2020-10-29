cf4f09bc6bf17b4a033d4904233a72de350201d8
Urs Brogle <ub@picturepark.com>
2020-10-29 15:14:39 +0100

    PP9-11952: Added translations.

--- src/picturepark-sdk-v1-angular/projects/share-viewer/src/app/translations/share-translations.ts
+++ src/picturepark-sdk-v1-angular/projects/share-viewer/src/app/translations/share-translations.ts
@@ -1,11 +1,10 @@
 export const shareTranslations = {
   Expired: {
     en: 'This share has expired and is no longer available. Please contact the person who sent you this link.',
-    de:
-      'Dieses Share ist abgelaufen und ist nicht mehr länger verfügbar. Bitte kontaktieren Sie die Person, die Ihnen diesen Link geschickt hat.',
-    fr: 'This share has expired and is no longer available. Please contact the person who sent you this link. (FR)',
-    es: 'This share has expired and is no longer available. Please contact the person who sent you this link. (ES)',
-    pt: 'This share has expired and is no longer available. Please contact the person who sent you this link. (PT)',
+    de: 'Dieses Share ist abgelaufen und nicht mehr länger verfügbar. Bitte kontaktieren Sie die Person, dwelche Ihnen diesen Link geschickt hat.',
+    fr: 'Cette collection partagée a expiré et n\'est plus disponible. Veuillez contacter la personne qui vous a envoyé ce lien s.v.p.',
+    es: 'Estos archivos compartidos han caducado y ya no están disponibles. Comuníquese con la persona que le envió este enlace.',
+    pt: 'Esta parthila expirou e já não está disponível. Por favor contacte a pessoa que lhe enviou esta ligação.',
   },
   Consent: {
     en: `By continuing to browse this website, you agree to the use of cookies to improve your user experience and to collect statistics about your visits.<br />
@@ -13,46 +12,46 @@ export const shareTranslations = {
     de: `Indem Sie die Website weiterhin verwenden, stimmen Sie der Nutzung von Cookies,
       zur Verbesserung Ihrer Nutzungserfahrung und um Statistiken von Ihrem Besuch zu sammeln, zu.<br />
       Lesen Sie die <a href="https://picturepark.com/de/terms/cookies" target="_blank">Cookie-Richtlinien</a>.`,
-    fr: `By continuing to browse this website, you agree to the use of cookies to improve your user experience and to collect statistics about your visits.<br />
-      Read the <a href="https://picturepark.com/en/terms/cookies" target="_blank">Cookie Policy</a>. (FR)`,
-    es: `By continuing to browse this website, you agree to the use of cookies to improve your user experience and to collect statistics about your visits.<br />
-      Read the <a href="https://picturepark.com/en/terms/cookies" target="_blank">Cookie Policy</a>. (ES)`,
-    pt: `By continuing to browse this website, you agree to the use of cookies to improve your user experience and to collect statistics about your visits.<br />
-      Read the <a href="https://picturepark.com/en/terms/cookies" target="_blank">Cookie Policy</a>. (PT)`,
+    fr: `En continuant à naviguer sur ce site web, vous acceptez l'utilisation de cookies pour améliorer votre expérience d'utilisateur et pour collecter des statistiques sur vos visites.<br />
+      Lire les <a href="https://picturepark.com/en/terms/cookies" target="_blank">informations sur les cookies</a>.`,
+    es: `Al continuar navegando en este sitio web, acepta el uso de cookies para mejorar su experiencia de usuario y recopilar estadísticas sobre sus visitas. <br />
+      Leer la <a href="https://picturepark.com/en/terms/cookies" target="_blank">información de las cookies</a>.`,
+    pt: `Ao continuar a navegar neste site, concorda com a utilização de cookies para melhorar a sua experiência de utilizador e para recolher estatísticas sobre as suas visitas. <br />
+      Leia a <a href="https://picturepark.com/en/terms/cookies" target="_blank">Informação do Cookie </a>.`,
   },
   OK: {
     en: 'OK',
     de: 'OK',
-    fr: 'OK (FR)',
-    es: 'OK (ES)',
-    pt: 'OK (PT)',
+    fr: 'OK',
+    es: 'Vale',
+    pt: 'OK',
   },
   CreationDate: {
     en: 'Creation date',
     de: 'Erstellungsdatum',
-    fr: 'Creation date (FR)',
-    es: 'Creation date (ES)',
-    pt: 'Creation date (PT)',
+    fr: 'Date de création',
+    es: 'Fecha de creación',
+    pt: 'Data de criação',
   },
   ExpirationDate: {
     en: 'Expiration date',
     de: 'Ablaufdatum',
-    fr: 'Expiration date (FR)',
-    es: 'Expiration date (ES)',
-    pt: 'Expiration date (PT)',
+    fr: 'Date d\'expiration',
+    es: 'Fecha de expiración',
+    pt: 'Data de expiração',
   },
   Creator: {
-    en: 'From',
-    de: 'Von',
-    fr: 'From (FR)',
-    es: 'From (ES)',
-    pt: 'From (PT)',
+    en: 'Sender',
+    de: 'Absender',
+    fr: 'Expéditeur',
+    es: 'Remitente',
+    pt: 'Remetente',
   },
   DownloadAll: {
     en: 'Download all',
     de: 'Alles herunterladen',
-    fr: 'Download all (FR)',
-    es: 'Download all (ES)',
-    pt: 'Download all (PT)',
+    fr: 'Tout télécharger',
+    es: 'Descargar todo',
+    pt: 'Baixar tudo',
   },
 };
