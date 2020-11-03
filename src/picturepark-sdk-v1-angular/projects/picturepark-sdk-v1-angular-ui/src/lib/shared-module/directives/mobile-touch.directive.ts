a013f74f65381367c9b1113c5614f81a267e3167
Ricardo Santos <ricardojoaosantos0@gmail.com>
2020-11-03 12:13:58 +0000

    Handle context menu interaction

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/shared-module/directives/mobile-touch.directive.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/shared-module/directives/mobile-touch.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Output, EventEmitter, HostListener, Input, OnInit } from '@angular/core';
+import { Directive, Output, EventEmitter, HostListener, Input, OnInit, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[ppMobileTouch]',
@@ -11,15 +11,24 @@ export class MobileTouchDirective {
   isLongPress = false;
   pressTimer: number;
 
+  constructor(private elementRef: ElementRef) {}
+
   @HostListener('touchstart')
   public onMouseDown(): void {
+    this.isLongPress = false;
     this.pressTimer = window.setTimeout(() => {
       this.isLongPress = true;
     }, this.pressTime);
   }
 
   @HostListener('click', ['$event'])
+  @HostListener('document:contextmenu ', ['$event'])
   public onMouseUp(event: MouseEvent): void {
+    const isDescendant = this.isDescendant(event.target as HTMLElement, this.elementRef.nativeElement);
+    if (event.type === 'contextmenu' && !isDescendant) {
+      return;
+    }
+
     clearTimeout(this.pressTimer);
     if (this.isLongPress) {
       this.ppPress.emit(event);
@@ -27,5 +36,21 @@ export class MobileTouchDirective {
       this.ppClick.emit(event);
     }
     this.isLongPress = false;
+  }
+
+  isDescendant(el: HTMLElement, parentElement: HTMLElement) {
+    let isChild = false;
+
+    if (el === parentElement) {
+      isChild = true;
+    }
+
+    while ((el = el.parentNode as HTMLElement)) {
+      if (el === parentElement) {
+        isChild = true;
+      }
+    }
+
+    return isChild;
   }
 }
