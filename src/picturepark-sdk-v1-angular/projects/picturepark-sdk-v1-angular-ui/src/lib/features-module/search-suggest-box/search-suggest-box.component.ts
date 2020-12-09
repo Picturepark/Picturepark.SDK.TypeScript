df14c82a9df3bbff3945ad19545a1396a1578270
tc4rdpp <tc@picturepark.com>
2020-12-09 23:51:40 +0000

    PP9-12054 - Removed old logic for search aggregator suggestions

--- src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/search-suggest-box/search-suggest-box.component.ts
+++ src/picturepark-sdk-v1-angular/projects/picturepark-sdk-v1-angular-ui/src/lib/features-module/search-suggest-box/search-suggest-box.component.ts
@@ -114,14 +114,8 @@ export class SearchSuggestBoxComponent extends BaseComponent implements OnInit {
   setSearchString(searchString: string | undefined) {
     const aggs: AggregatorBase[] = [];
     this.facade.searchRequestState.aggregators.forEach((aggregation) => {
-      if (!aggregation.uiBehavior?.enableSuggestions) {
-        return;
-      }
-
-      const expanded = this.expandAggregator(aggregation);
-      if (expanded.searchFields && expanded.searchFields.length) {
-        expanded.searchString = searchString;
-        aggs.push(aggregation);
+      if (aggregation.uiBehavior?.enableSuggestions) {
+        aggs.push(aggregation)
       }
     });
 
