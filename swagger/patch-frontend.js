ade00fc885ebaafc0b7f32af30ea96af35edd81e
Urs Brogle <ub@picturepark.com>
2021-09-16 15:58:06 +0200

    PP9-13774: Patch Frontend.json to exclude everything but ShareAccess Controller.

--- swagger/patch-frontend.js
+++ swagger/patch-frontend.js
@@ -2,7 +2,24 @@ const fs = require('fs')
 
 try {
   const data = fs.readFileSync('./swagger/Frontend.json', 'utf8').trim();
-  console.log(JSON.parse(data))
+  const schema = JSON.parse(data);
+  const paths = schema.paths;
+  const updatedPath = {};
+  Object.keys(paths).forEach(pathKey => {
+      const operations = {};
+      Object.keys(paths[pathKey]).forEach(operationKey => {
+          const operation = paths[pathKey][operationKey];
+        if(operation.tags.includes("ShareAccess")) {
+            console.log(operation)
+            operations[operationKey] = operation;
+        }
+      });
+      if(Object.keys(operations).length) {
+        updatedPath[pathKey] = operations;
+      }
+  });
+  schema.paths = updatedPath;
+  fs.writeFileSync('./swagger/Frontend.json', JSON.stringify(schema, undefined, '\t'), 'utf8');
 } catch (err) {
   console.error(err)
 }
