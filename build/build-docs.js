b1218fc7e201a0c85c0646aaf7dab37ac1d60ac8
Urs Brogle <urs.brogle@fotoware.com>
2024-07-24 15:09:34 +0200

    PP9-20529: Fix docs creation of v2

--- build/build-docs.js
+++ build/build-docs.js
@@ -23,3 +23,14 @@ runTypeDoc(
   path.join(rootDir, '../docs/picturepark-sdk-v1-pickers/API.md'),
   'picturepark-sdk-v1-pickers API'
 );
+
+runTypeDoc(
+  path.join(rootDir, '../src/picturepark-sdk-v2-angular/projects/picturepark-sdk-v2-angular/src/public_api.ts'),
+  path.join(rootDir, '../src/picturepark-sdk-v2-angular/projects/picturepark-sdk-v2-angular/tsconfig.lib.json'),
+  path.join(rootDir, '../docs/picturepark-sdk-v2-angular/api'),
+  path.join(rootDir, '../docs/picturepark-sdk-v2-angular/API.md'),
+  'picturepark-sdk-v2-angular API'
+);
+
+
+// cmd /c call "node_modules/.bin/typedoc" "%~dp0\..\src\picturepark-sdk-v1-angular\projects\picturepark-sdk-v1-angular\src\public_api.ts" --out "%~dp0\..\docs\picturepark-sdk-v1-angular\api" --readme "%~dp0\..\docs\picturepark-sdk-v1-angular\API.md" --theme default --tsconfig "%~dp0\..\src\picturepark-sdk-v1-angular\projects\picturepark-sdk-v1-angular\tsconfig.lib.json" --excludePrivate --name "@picturepark/sdk-v1-angular API"
