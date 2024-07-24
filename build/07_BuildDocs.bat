95a63cb7c87abcafbc5765876783af4af3f55084
Urs Brogle <urs.brogle@fotoware.com>
2024-07-24 15:09:34 +0200

    PP9-20529: Fix docs creation of v2

--- build/07_BuildDocs.bat
+++
@@ -1,6 +0,0 @@
-cmd /c call "node_modules/.bin/typedoc" "%~dp0\..\src\picturepark-sdk-v1-fetch\src\index.ts" --tsconfig  "%~dp0\..\src\picturepark-sdk-v1-fetch\tsconfig.json" --out "%~dp0\..\docs\picturepark-sdk-v1-fetch\api" --readme "%~dp0\..\docs\picturepark-sdk-v1-fetch\API.md" --theme default --name "picturepark-sdk-v1-fetch API"
-cmd /c call "node_modules/.bin/typedoc" "%~dp0\..\src\picturepark-sdk-v1-pickers\src\index.ts" --tsconfig  "%~dp0\..\src\picturepark-sdk-v1-pickers\tsconfig.json" --out "%~dp0\..\docs\picturepark-sdk-v1-pickers\api" --readme "%~dp0\..\docs\picturepark-sdk-v1-pickers\API.md" --theme default --name "picturepark-sdk-v1-pickers API"
-
-pushd "%~dp0/../src/picturepark-sdk-v1-angular"
-popd
-cmd /c call "node_modules/.bin/typedoc" "%~dp0\..\src\picturepark-sdk-v1-angular\projects\picturepark-sdk-v1-angular\src\public_api.ts" --out "%~dp0\..\docs\picturepark-sdk-v1-angular\api" --readme "%~dp0\..\docs\picturepark-sdk-v1-angular\API.md" --theme default --tsconfig "%~dp0\..\src\picturepark-sdk-v1-angular\projects\picturepark-sdk-v1-angular\tsconfig.lib.json" --excludePrivate --name "@picturepark/sdk-v1-angular API"
